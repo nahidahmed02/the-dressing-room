684ec4b7d9cf29de096bdc56a27fbe4480c0ad5e
unknown <ahmednahid1995@gmail.com>
2022-05-07 23:54:36 +0600

    third Q&A added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -14,6 +14,7 @@ const Blogs = () => {
                 <br />
                 3. JavaScript is capable of adding HTML. NodeJs is not capable of it.
             </p>
+
             <h2 className='Q-A mt-5'>Q: What is the difference between SQL and NoSQL?</h2>
             <p className='Q-A'>A: Some of the differences between SQL and NoSQL are given below-
                 <br />
@@ -25,6 +26,12 @@ const Blogs = () => {
                 3. Example of SQL database are - MySQL, Oracle, Sqlite etc.
                 <br />
                 Example of NoSQL database are - MongoDB, BigTable, Redis etc.
+            </p>
+
+            <h2 className='Q-A mt-5'>Q: What is the purpose of JWT and how does it work?</h2>
+            <p className='Q-A'> A: JWT (JSON Web Token) sends securely transmitted information from client-side to server-side. JWT prevents an unauthorized user to access data.
+                <br />
+                When a user logged in, jwt creates a token from server. That token comes to client from server. When the user sends a request to backend, a copy of the token is send to the server for authorization with bearer. Then server verifies the token and responds to the client request.
             </p>
         </div>
     );
