cefbee9346690a4ee54376fc38bc928f11319c18
unknown <ahmednahid1995@gmail.com>
2022-05-07 13:49:11 +0600

    custom link added

---
+++ src/components/CustomLink/CustomLink.js
@@ -0,0 +1,25 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+function CustomLink({ children, to, ...props }) {
+    let resolved = useResolvedPath(to);
+    let match = useMatch({ path: resolved.pathname, end: true });
+
+    return (
+        <div>
+            <Link
+                style={{
+                    color: match ? "orange" : "grey",
+                    textDecoration: 'none',
+                    fontWeight: 'bold',
+                    marginRight: '20px'
+                }}
+                to={to}
+                {...props}
+            >
+                {children}
+            </Link>
+        </div>
+    );
+}
+
+export default CustomLink;
