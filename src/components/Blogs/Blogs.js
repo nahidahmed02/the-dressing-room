655242692af3b1109c086efebe78c389c29074dc
unknown <ahmednahid1995@gmail.com>
2022-05-07 23:11:13 +0600

    second Q&A added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -14,6 +14,18 @@ const Blogs = () => {
                 <br />
                 3. JavaScript is capable of adding HTML. NodeJs is not capable of it.
             </p>
+            <h2 className='Q-A mt-5'>Q: What is the difference between SQL and NoSQL?</h2>
+            <p className='Q-A'>A: Some of the differences between SQL and NoSQL are given below-
+                <br />
+                <br />
+                1. SQL databases have fixed or static or predefined schema. On the other side, NoSQL databases have dynamic schema.
+                <br />
+                2. Though SQL databases are vertically scalable but NoSQL databases are horizontally scalable.
+                <br />
+                3. Example of SQL database are - MySQL, Oracle, Sqlite etc.
+                <br />
+                Example of NoSQL database are - MongoDB, BigTable, Redis etc.
+            </p>
         </div>
     );
 };
