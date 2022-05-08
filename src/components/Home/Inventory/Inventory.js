71f9e45226c4c44f187b69d6111644a22489b128
unknown <ahmednahid1995@gmail.com>
2022-05-08 23:47:55 +0600

    manage inventories button implimented

--- src/components/Home/Inventory/Inventory.js
+++ src/components/Home/Inventory/Inventory.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import useItems from '../../../hooks/useItems';
 import ManageSingleItem from '../../ManageSingleItem/ManageSingleItem';
+import { useNavigate } from 'react-router-dom';
 
 
 // will show 6 items in the home page
 const Inventory = () => {
+    const navigate = useNavigate();
     const [items] = useItems();
     return (
         <div className='container'>
@@ -16,6 +18,9 @@ const Inventory = () => {
                         item={item}
                     ></ManageSingleItem>)
                 }
+            </div>
+            <div className='text-center mb-5'>
+                <button onClick={() => navigate('/manageitems')} className='btn btn-info btn-lg px-5'>Manage Inventories</button>
             </div>
         </div>
     );
