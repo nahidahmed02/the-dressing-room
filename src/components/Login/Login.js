a522eb29af45c6bd51b1d149c36401ed1e764094
unknown <ahmednahid1995@gmail.com>
2022-05-09 19:00:25 +0600

    login implimented

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,77 @@
-import React from 'react';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import auth from '../../firebase.init';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const navigate = useNavigate();
+
+    const location = useLocation();
+    const from = location.state?.from?.pathname || "/";
+
+    if (user) {
+        navigate(from, { replace: true });
+    }
+    // get the email,password and submit
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+
+    const handleUserSignIn = event => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password);
+    }
+    // forgot password
+    const resetPassword = () => {
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                toast('Email sent')
+            })
+    }
     return (
-        <div>
-            <h2>login</h2>
+        <div className='card w-50 mx-auto mt-4'>
+            <h2 className='text-center fw-bold text-info fst-italic mt-3'>Login</h2>
+            <form onSubmit={handleUserSignIn} className='w-50 mx-auto'>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <input onBlur={handleEmailBlur} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <input onBlur={handlePasswordBlur} type="password" className="form-control" id="exampleInputPassword1" required />
+                </div>
+
+                <p style={{ color: 'red' }}>{error?.message}</p>
+                {
+                    loading && <p>Please Wait...</p>
+                }
+
+                <div className='text-center d-grid'>
+                    <button type="submit" className="btn btn-outline-success">Login</button>
+                </div>
+                <div className='text-center'>
+                    <button onClick={resetPassword} className='btn btn-link'>Forgot Password</button>
+                </div>
+            </form>
+            <p className='text-center mt-2'>
+                Don't have an account?  <Link to='/signup' className='form-link'>Create an account</Link>
+            </p>
         </div>
     );
 };
